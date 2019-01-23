d0f981187b1e9795883f3c00278466624bb32106
Park Juhyung <jh@kodebox.io>
2019-01-23 17:37:22 +0900

    Fit to the version 73dcf60ed16399c3e95d4f2fce6b08a0dcb0856c of Indexer

--- src/index.ts
+++ src/index.ts
@@ -3,213 +3,126 @@ import * as _ from "lodash";
 export type NetworkId = "cc" | "tc" | "sc" | "wc";
 
 export interface BlockDoc {
+    hash: string;
     parentHash: string;
     timestamp: number;
     number: number;
     author: string;
     extraData: Buffer;
-    parcelsRoot: string;
+    transactionsRoot: string;
     stateRoot: string;
     invoicesRoot: string;
     score: string;
     seal: Buffer[];
-    hash: string;
-    parcels: ParcelDoc[];
-    /* custom field for indexing */
     miningReward: string;
-}
-
-export interface ParcelDoc {
-    blockNumber?: number | null;
-    blockHash?: string | null;
-    parcelIndex?: number | null;
-    seq: number;
-    fee: string;
-    networkId: NetworkId;
-    sig: string;
-    hash: string;
-    action: ActionDoc;
-    /* custom field for indexing */
-    signer: string;
-    timestamp: number;
-}
-
-export type ActionDoc =
-    | AssetTransactionDoc
-    | PaymentDoc
-    | SetRegularKeyDoc
-    | CreateShardDoc
-    | SetShardOwnersDoc
-    | SetShardUsersDoc;
-
-export interface AssetTransactionDoc {
-    action: "assetTransaction";
-    transaction: TransactionDoc;
-    /* custom field for indexing */
-    invoice?: boolean | null;
-    errorType?: string | null;
-}
-
-export interface PaymentDoc {
-    action: "payment";
-    receiver: string;
-    amount: string;
-    /* custom field for indexing */
-    invoice?: boolean | null;
-    errorType?: string | null;
-}
-
-export interface SetRegularKeyDoc {
-    action: "setRegularKey";
-    key: string;
-    /* custom field for indexing */
-    invoice?: boolean | null;
-    errorType?: string | null;
-}
-
-export interface CreateShardDoc {
-    action: "createShard";
-    /* custom field for indexing */
-    invoice?: boolean | null;
-    errorType?: string | null;
-}
-
-export interface SetShardOwnersDoc {
-    action: "setShardOwners";
-    shardId: number;
-    owners: string[];
-    /* custom field for indexing */
-    invoice?: boolean | null;
-    errorType?: string | null;
-}
-
-export interface SetShardUsersDoc {
-    action: "setShardUsers";
-    shardId: number;
-    users: string[];
-    /* custom field for indexing */
-    invoice?: boolean;
-    errorType?: string;
+    transactions?: TransactionDoc[];
 }
 
 export type TransactionDoc =
-    | AssetMintTransactionDoc
-    | AssetTransferTransactionDoc
-    | AssetComposeTransactionDoc
-    | AssetDecomposeTransactionDoc;
+    | PayTransactionDoc
+    | MintAssetTransactionDoc
+    | TransferAssetTransactionDoc
+    | ComposeAssetTransactionDoc
+    | DecomposeAssetTransactionDoc
+    | WrapCCCTransactionDoc
+    | UnwrapCCCTransactionDoc
+    | SetRegularKeyTransactionDoc
+    | CreateShardTransactionDoc
+    | SetShardOwnersTransactionDoc
+    | StoreTransactionDoc
+    | RemoveTransactionDoc
+    | CustomTransactionDoc;
 
-export interface AssetSchemeDoc {
-    metadata: Metadata;
-    registrar?: string | null;
-    amount?: string | null;
-    networkId?: string | null;
-    shardId?: number | null;
+export interface TransactionBaseDoc {
+    hash: string;
+    blockNumber?: number | null;
+    blockHash?: string | null;
+    tracker?: string | null;
+    transactionIndex?: number | null;
+    seq: number;
+    fee: string;
+    networkId: string;
+    sig: string;
+    signer: string;
+    invoice?: boolean | null;
+    errorType?: string | null;
+    timestamp?: number | null;
+    isPending: boolean;
+    pendingTimestamp?: number | null;
 }
 
-export interface AssetDoc {
-    assetType: string;
-    lockScriptHash: string;
-    parameters: Buffer[];
-    amount: string;
-    transactionHash: string;
-    transactionOutputIndex: number;
+export interface PayTransactionDoc extends TransactionBaseDoc {
+    type: "pay";
+    pay: {
+        transactionHash: string;
+        receiver: string;
+        quantity: string;
+    };
 }
 
-export interface UTXO {
-    asset: AssetDoc;
-    assetScheme: AssetSchemeDoc;
-    blockNumber: number;
-    parcelIndex: number;
-}
-
-export interface AggsUTXO {
-    assetType: string;
-    assetScheme: AssetSchemeDoc;
-    totalAssetQuantity: number;
-    utxoQuantity: number;
-}
-
-export interface AssetMintTransactionDoc {
-    type: "assetMint";
-    data: {
-        output: AssetMintOutputDoc;
+export interface MintAssetTransactionDoc extends TransactionBaseDoc {
+    type: "mintAsset";
+    mintAsset: {
         networkId: string;
         shardId: number;
-        metadata: Metadata;
-        registrar?: string | null;
-        /* custom field for indexing */
-        hash: string;
-        timestamp: number;
-        assetName: string;
-        parcelHash: string;
-        blockNumber: number;
-        parcelIndex: number;
-        invoice?: boolean | null;
-        errorType?: string | null;
+        metadata: string;
+        approver?: string | null;
+        administrator?: string | null;
+        allowedScriptHashes: string[];
+        approvals: string[];
+
+        lockScriptHash: string;
+        parameters: string[];
+        supply: string;
+
+        assetName?: string;
+        assetType: string;
+        recipient: string;
     };
 }
 
-export interface AssetMintOutputDoc {
-    lockScriptHash: string;
-    parameters: Buffer[];
-    amount?: string | null;
-    recipient?: string | null;
-    /* custom field for indexing */
-    assetType: string;
-}
-
-export interface AssetTransferTransactionDoc {
-    type: "assetTransfer";
-    data: {
+export interface TransferAssetTransactionDoc {
+    type: "transferAsset";
+    transferAsset: {
         networkId: string;
-        burns: AssetTransferInputDoc[];
-        inputs: AssetTransferInputDoc[];
-        outputs: AssetTransferOutputDoc[];
-        /* custom field for indexing */
-        hash: string;
-        timestamp: number;
-        parcelHash: string;
-        blockNumber: number;
-        parcelIndex: number;
-        invoice?: boolean | null;
-        errorType?: string | null;
+        approvals: string[];
+        inputs?: AssetTransferInputDoc[];
+        burns?: AssetTransferInputDoc[];
+        outputs?: AssetTransferOutputDoc[];
     };
 }
 
-export interface AssetComposeTransactionDoc {
-    type: "assetCompose";
-    data: {
+export interface ComposeAssetTransactionDoc {
+    type: "composeAsset";
+    composeAsset: {
         networkId: string;
         shardId: number;
-        metadata: Metadata;
-        registrar?: string | null;
-        output: AssetMintOutputDoc;
-        inputs: AssetTransferInputDoc[];
-        /* custom field for indexing */
-        hash: string;
-        timestamp: number;
-        parcelHash: string;
-        blockNumber: number;
-        parcelIndex: number;
-        invoice?: boolean | null;
-        errorType?: string | null;
+        metadata: string;
+        approver?: string | null;
+        administrator?: string | null;
+        allowedScriptHashes: string[];
+
+        approvals: string[];
+
+        lockScriptHash: string;
+        parameters: string[];
+        supply: string;
+
+        assetName?: string;
+        assetType: string;
+        recipient: string;
+
+        inputs?: AssetTransferInputDoc[];
     };
 }
 
-export interface AssetDecomposeTransactionDoc {
-    type: "assetDecompose";
-    data: {
+export interface DecomposeAssetTransactionDoc {
+    type: "decomposeAsset";
+    decomposeAsset: {
+        networkId: string;
+        approvals: string[];
         input: AssetTransferInputDoc;
         outputs: AssetTransferOutputDoc[];
-        networkId: string;
-        /* custom field for indexing */
-        hash: string;
-        timestamp: number;
-        parcelHash: string;
-        blockNumber: number;
-        parcelIndex: number;
-        invoice?: boolean | null;
-        errorType?: string | null;
     };
 }
 
@@ -222,42 +135,41 @@ export interface Timelock {
 export interface AssetTransferInputDoc {
     prevOut: AssetOutPointDoc;
     timelock?: Timelock | null;
+    owner?: string | null;
+    assetType: string;
     lockScript: Buffer;
     unlockScript: Buffer;
 }
 
 export interface AssetOutPointDoc {
-    transactionHash: string;
+    tracker: string;
     index: number;
     assetType: string;
     assetScheme: AssetSchemeDoc;
-    amount: string;
-    /* custom field for indexing */
+    quantity: string;
     owner?: string | null;
-    lockScriptHash: string;
-    parameters: Buffer[];
+    lockScriptHash?: string | null;
+    parameters?: string[] | null;
+}
+
+export interface AssetSchemeDoc {
+    assetType: string;
+    metadata: string;
+    approver?: string | null;
+    administrator?: string | null;
+    allowedScriptHashes: string[];
+    supply?: string | null;
+    networkId?: string | null;
+    shardId?: number | null;
 }
 
 export interface AssetTransferOutputDoc {
     lockScriptHash: string;
-    parameters: Buffer[];
+    parameters: string[];
     assetType: string;
-    amount: string;
-    /* custom field for indexing */
+    quantity: string;
     owner?: string | null;
     assetScheme: AssetSchemeDoc;
-}
-
-export interface PendingParcelDoc {
-    parcel: ParcelDoc;
-    status: string;
-    timestamp: number;
-}
-
-export interface PendingTransactionDoc {
-    transaction: TransactionDoc;
-    status: string;
-    timestamp: number;
 }
 
 export interface Metadata {
@@ -265,4 +177,98 @@ export interface Metadata {
     description?: string;
     icon_url?: string;
     gateway?: string;
+}
+
+export interface WrapCCCTransactionDoc extends TransactionBaseDoc {
+    type: "wrapCCC";
+    wrapCCC: {
+        shardId: number;
+        lockScriptHash: string;
+        parameters: string[];
+        quantity: string;
+        recipient: string;
+    };
+}
+
+export interface UnwrapCCCTransactionDoc extends TransactionBaseDoc {
+    type: "unwrapCCC";
+    unwrapCCC: {
+        burn?: AssetTransferInputDoc;
+    };
+}
+
+export interface SetRegularKeyTransactionDoc extends TransactionBaseDoc {
+    type: "setRegularKey";
+    setRegularKey: {
+        key: string;
+    };
+}
+
+export interface CreateShardTransactionDoc extends TransactionBaseDoc {
+    type: "createShard";
+}
+
+export interface SetShardOwnersTransactionDoc extends TransactionBaseDoc {
+    type: "setShardOwners";
+    setShardOwners: {
+        transactionHash: string;
+        shardId: number;
+        owners: string[];
+    };
+}
+
+export interface StoreTransactionDoc extends TransactionBaseDoc {
+    type: "store";
+    store: {
+        content: string;
+        certifier: string;
+        signature: string;
+    };
+}
+
+export interface RemoveTransactionDoc extends TransactionBaseDoc {
+    type: "remove";
+    remove: {
+        textHash: string;
+        signature: string;
+    };
+}
+
+export interface CustomTransactionDoc extends TransactionBaseDoc {
+    type: "custom";
+    custom: {
+        handlerId: number;
+        content: string;
+    };
+}
+
+export interface AccountDoc {
+    address: string;
+    balance: string;
+    seq: number;
+}
+
+export interface UTXODoc {
+    address: string;
+    assetType: string;
+    lockScriptHash: string;
+    parameters: string[];
+    quantity: string;
+    transactionHash: string;
+    transactionOutputIndex: number;
+    usedTransactionHash: string | null;
+    assetScheme: AssetSchemeDoc;
+    blockNumber: number;
+}
+
+export interface AggsUTXODoc {
+    assetType: string;
+    totalAssetQuantity: number;
+    utxoQuantity: number;
+}
+
+export interface UTXOSnapshotDoc {
+    snapshotId: string;
+    blockNumber: number;
+    snapshot: UTXODoc[];
 }
