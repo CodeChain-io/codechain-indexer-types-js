bc15ef2fa13dd570922b31aa5b4699a5e0ec29ce
JinGyeong Jeong <jjg@kodebox.io>
2019-04-23 20:18:52 +0900

    Add transaction.type to BalanceHistoryDoc

--- src/balanceHistory.ts
+++ src/balanceHistory.ts
@@ -1,7 +1,12 @@
+import { TransactionDoc } from "./transaction";
+
 export interface BalanceHistoryDoc {
     address: string;
     change: string;
     blockNumber: number;
     reason: "fee" | "author" | "stake" | "tx" | "initial_distribution";
     transactionHash: string;
+    transaction: {
+        type: TransactionDoc["type"];
+    };
 }
